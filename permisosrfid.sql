a2399c0567ee5dfe439e6afab5a20dfbcbf98be6
Alvisex <alvise.ld@gmail.com>
2018-06-01 13:16:32 -0500

    Sql con tabla de registro

--- permisosrfid.sql
+++ permisosrfid.sql
@@ -1,10 +1,6 @@
 CREATE TABLE permisos (id_tarjeta BIGINT NOT NULL PRIMARY KEY, usuario VARCHAR(100), permiso BOOL NOT NULL);
 
-
 INSERT INTO permisos VALUES (441294522346, 'Tarjeta Blanca', 1);
-
-
-
 
 
 USE datos_iot;
@@ -14,10 +10,18 @@ SELECT * FROM permisos;
 SELECT permiso FROM permisos WHERE id_tarjeta=
 
 
+
 DELETE FROM permisos WHERE id_tarjeta=990154079132
 
-
-
-
-
 DROP TABLE permisos;
+
+
+
+
+
+
+
+CREATE TABLE registro (id_tarjeta BIGINT NOT NULL, usuario VARCHAR(100), fecha DATETIME);
+
+
+INSERT INTO registro VALUES ()
